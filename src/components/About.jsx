d280e6dc9e50cf6a053f33ffa423a014b225b563
Nidhi Prakash <np562019@gmail.com>
2022-07-23 16:30:35 +0530

    updated-again

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,10 +5,10 @@ const About = () => {
     <div
       name="about"
       id="about"
-      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white "
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
-        <div className="pb-8">
+        <div className="pt-[100px]">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
             About
           </p>
